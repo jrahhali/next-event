a1f2bce55088cf90af18c824c7f28897b4cda78d
jrahhali <rahhali@live.com>
2015-08-07 21:22:01 -0300

    - installed whitespace sanitizer, and used it on all files. - minor refactoring of code

--- test/event.js
+++ test/event.js
@@ -11,11 +11,11 @@ describe("Event", function() {
             assert(event._listeners.length === 1);
             event.add(function(){});
             assert(event._listeners.length === 2);
-        });        
+        });
     });
-    
+
     describe("remove()", function() {
-        it("should remove the supplied listener", function() {
+        it("should remove listeners", function() {
             var event = new Event();
             var callOrder = "";
             var listener1 = function() { callOrder += "1"; };
@@ -26,19 +26,19 @@ describe("Event", function() {
             event.fire();
             assert.equal(callOrder, "2");
         });
-    });    
-    
+    });
+
     describe("clear()", function() {
-        it("should clear the event of all listeners", function() {
+        it("should remove all listeners", function() {
             var event = new Event();
             event.add(function(){});
             event.clear();
             assert(event._listeners.length === 0);
         });
     });
-    
 
-    
+
+
     describe("fire()", function() {
         it("should notify listeners in the order they were added", function() {
             var event = new Event();
@@ -48,25 +48,16 @@ describe("Event", function() {
             event.fire();
             assert(callOrder === "12");
         });
-        
-        it("should call its listeners with 'sender' and 'data' arguments", function() {
-            var event = new Event();
-            var sender;
-            var data;
-            event.add(function() { 
-                sender = arguments[0];
-                data = arguments[1];
-            });
-            event.fire("message");
-            assert(sender === event && data === "message");
-        });        
-        
-        it("should finish notifying all listeners before another event notifies theirs", function() {
+
+        it("should fire events in the order that they were triggered", function() {
+
             var event1 = new Event();
             var event2 = new Event();
             var event3 = new Event();
             var callOrder = "";
-            
+
+
+            var sender = this;
             event1.add(function() {
                 callOrder += "1";
                 event2.fire();
@@ -75,19 +66,31 @@ describe("Event", function() {
                  callOrder += "2";
             });
             event2.add(function() {
-                callOrder += "3"; 
-                event3.fire();                
+                callOrder += "3";
+                event3.fire();
             });
             event2.add(function() {
-                callOrder += "4"; 
+                callOrder += "4";
             });
             event3.add(function() {
-                callOrder += "5"; 
+                callOrder += "5";
             });
-            
+
             event1.fire();
-            
+
             assert.equal(callOrder, "12345");
-        });        
+        });
+
+        it("should call listeners with 'sender' and 'data' arguments", function() {
+            var event = new Event();
+            var sender;
+            var data;
+            event.add(function() {
+                sender = arguments[0];
+                data = arguments[1];
+            });
+            event.fire(this, "message");
+            assert(sender === this && data === "message");
+        });
     });
 });
